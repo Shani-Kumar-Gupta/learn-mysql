608752868d23f5e0468e76267f04ee43d749546a
Shani-Kumar-Gupta <shani.gupta_cse@outlook.com>
2024-10-04 22:18:22 +0530

    first commit

---
+++ student_info.sql
@@ -0,0 +1,10 @@
+INSERT INTO student (name, university, branch, year, course) VALUES 
+('Shani Kumar Gupta', 'GLA University', 'Computer Science', 2017, 'B.Tech'),
+('Priyanshu Jain', 'GLA University', 'Computer Science', 2017, 'B.Tech'),
+('Priti Gupta', 'GNIOT', 'IT', 2021, 'B.Tech'),
+('Aman Kumar', 'GLA University', 'Pharmacy', 2017, 'B.Pham'),
+('Vivek Kumar', 'GL Bajaj University', 'Electrical and Electronics', 2017, 'B.Tech'),
+('Sweta Gupta', 'GLA University', 'IT', 2020, 'B.Tech'),
+('Shishankar Singh', 'United University', 'Mechanical', 2017, 'B.Tech'),
+('Keshavi Aggrawal', 'GLA University', 'Computer Science', 2017, 'B.Tech'),
+('Aman Kumar', 'GLA University', 'Computer Science', 2017, 'B.Tech');
